86e8154fcd6e02692584afa46bcca31d6efe7005
Madushan Sandaruwan <madushansandaru1@gmail.com>
2022-07-19 10:14:01 +0530

    Setup the Angular application

---
+++ src/main/java/com/example/angularspringboot/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.angularspringboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+//    @RequestMapping("/")
+//    public String greet() {
+//        return "welcome";
+//    }
+
+}
